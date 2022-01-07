6e978ac190a25c7e942943ffed7769c1828934a8
saisrinid <srini.dola@gmail.com>
2022-01-06 18:46:22 -0700

    Added read.py and write.py and added logic to app.py and config.py to handle the read and write to MySQL

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from mysql import connector as mc
 from mysql.connector import errorcode as ec
+#import psycopg2 as psy
 
 
 def get_mysql_conn(DB_USER,DB_PASS,DB_HOST,DB_NAME):
@@ -16,8 +17,30 @@ def get_mysql_conn(DB_USER,DB_PASS,DB_HOST,DB_NAME):
             print(error)
     return conn
 
-
-
+#def get_postgres_conn(DB_USER,DB_PASS,DB_HOST,DB_NAME):
+#    try:
+#        conn=psy.connect(user=DB_USER,
+#                        password=DB_PASS,
+#                        host=DB_HOST,
+#                        database=DB_NAME)
+#    except mc.Error as error:
+#        if error.errno==ec.ER_ACCESS_DENIED_ERROR:
+#            print('Invalid Credentials')
+#        else:
+#            print(error)
+#    return conn
+#
+#def get_connection(DB_TYPE,DB_USER,DB_PASS,DB_HOST,DB_NAME):
+#    if DB_TYPE=='mysql':
+#        conn=get_mysql_conn(DB_USER,DB_PASS,DB_HOST,DB_NAME)
+#    elif DB_TYPE=='postgres':
+#        conn=get_postgres_conn(DB_USER,DB_PASS,DB_HOST,DB_NAME)
+#
+#    return conn
+#
+#
+#
+#
 def get_tables(path):
     table_list=pd.read_csv(path,sep=':')
     tables=table_list.loc[table_list['loaded']=='yes','table_name']
--- write.py
+++ write.py
@@ -2,11 +2,12 @@ from utils import get_mysql_conn
 from mysql import connector as mc
 
 def write_data(tgt_db,src_db,table_name):
-    conn=mc.connect(user=tgt_db['DB_USER'],
-                    password=tgt_db['DB_PASS'],
-                    host=tgt_db['DB_HOST'],
-                    database=tgt_db['DB_NAME']
-                    )
+#    conn=mc.connect(user=tgt_db['DB_USER'],
+#                    password=tgt_db['DB_PASS'],
+#                    host=tgt_db['DB_HOST'],
+#                    database=tgt_db['DB_NAME']
+#                    )
+    conn = get_mysql_conn(tgt_db['DB_USER'], tgt_db['DB_PASS'], tgt_db['DB_HOST'], tgt_db['DB_NAME'])
     cursor=conn.cursor()
     src_db_name=src_db['DB_NAME']
     query= f'insert into {table_name} select * from {src_db_name}.{table_name}'
